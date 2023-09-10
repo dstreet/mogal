c3895a3f05f93bbe9817eadfc2607bfd32a2a06e
David Street <street.davide@gmail.com>
2023-09-10 15:04:42 -0400

    Update gql generation

--- ui/codegen.ts
+++ ui/codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli'
  
 const config: CodegenConfig = {
-  schema: 'http://localhost:8080/graphql',
+  schema: '../schema.graphql',
   documents: ['src/**/*.ts'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
